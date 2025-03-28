71f15fc36d3479eadd466e90e5834702857b9a63
FalesDev <fabriciorodriguezavalos043@gmail.com>
2025-03-28 18:57:02 -0500

    Ajustes para produccion

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// Si trabajs en local es http://localhost:8080, pero si ya lo subiste a un servidor, colocalo el link generado por el servidor
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "https://cyberblog-latest.onrender.com",
         changeOrigin: true,
-      }
-    }
-  }
-})
-
+      },
+    },
+  },
+});
