2ab05fe5e430c55af5ae16154e961264307a4391
FalesDev <fabriciorodriguezavalos043@gmail.com>
2025-03-28 19:19:29 -0500

    Cambios para produccion en vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // Si trabajs en local es http://localhost:8080, pero si ya lo subiste a un servidor, colocalo el link generado por el servidor
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/cyberBlog",
   server: {
     proxy: {
       "/api": {
